ed2fab859eca9dcd364631f630a08bdad8c70d2b
BimalKatuwal <bimal.079csit09@godawari.edu.np>
2025-08-12 09:32:05 +0545

    first commit

---
+++ todobimal/Controllers/textController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace todobimal.Controllers
+{
+    public class textController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ todobimal/Models/TasksList.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace todobimal.Models
+{
+    public class TasksList
+    {
+        [Required]
+        [Display(Name ="Task")]
+        public string Task {  get; set; }
+
+        [Required]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "status")]
+        public string Status { get; set; }
+    }
+}
